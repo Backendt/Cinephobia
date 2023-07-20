7194a68adfa030eb7c9354aa95b247ff7ea2fc87
Backendt <matisse.beckandt@gmail.com>
2023-07-20 21:13:10 +0200

    Initial commit

---
+++ cinephobia-webapp/src/main/java/fr/backendt/cinephobia/CinephobiaApplication.java
@@ -0,0 +1,13 @@
+package fr.backendt.cinephobia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinephobiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinephobiaApplication.class, args);
+	}
+
+}
---
+++ cinephobia-webapp/src/test/java/fr/backendt/cinephobia/CinephobiaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.backendt.cinephobia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinephobiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
