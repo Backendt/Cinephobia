0af592a8866a7ac3e98e9b2e4a8df40ddf27a2ff
Backendt <matisse.beckandt@gmail.com>
2023-07-26 01:48:34 +0200

    Added Warn repository and tests

--- cinephobia-webapp/src/main/java/fr/backendt/cinephobia/repositories/WarnRepository.java
+++ cinephobia-webapp/src/main/java/fr/backendt/cinephobia/repositories/WarnRepository.java
@@ -1,2 +1,14 @@
-package fr.backendt.cinephobia.repositories;public class WarnRepository {
+package fr.backendt.cinephobia.repositories;
+
+import fr.backendt.cinephobia.models.Warn;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface WarnRepository extends JpaRepository<Warn, Long> {
+
+    List<Warn> findAllByMediaId(Long id);
+
 }
--- cinephobia-webapp/src/test/java/fr/backendt/cinephobia/repositories/WarnRepositoryTests.java
+++ cinephobia-webapp/src/test/java/fr/backendt/cinephobia/repositories/WarnRepositoryTests.java
@@ -1,2 +1,101 @@
-package fr.backendt.cinephobia.repositories;public class WarnRepositoryTests {
+package fr.backendt.cinephobia.repositories;
+
+import fr.backendt.cinephobia.models.Media;
+import fr.backendt.cinephobia.models.Trigger;
+import fr.backendt.cinephobia.models.Warn;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Transactional
+@SpringBootTest
+class WarnRepositoryTests {
+
+    @Autowired
+    private MediaRepository mediaRepository;
+
+    @Autowired
+    private TriggerRepository triggerRepository;
+
+    @Autowired
+    private WarnRepository repository;
+
+    // Create
+    @Test
+    void createWarnTest() {
+        // GIVEN
+        Trigger trigger = triggerRepository.findById(1L).orElseThrow();
+        Media media = mediaRepository.findById(1L).orElseThrow();
+        Warn warn = new Warn(trigger, media, 9);
+        Warn result;
+
+        // WHEN
+        result = repository.save(warn);
+
+        // THEN
+        assertThat(result.getId()).isNotNull();
+    }
+
+    @Test
+    void getWarnByIdTest() {
+        // GIVEN
+        Long warnId = 1L;
+        Optional<Warn> result;
+
+        // WHEN
+        result = repository.findById(warnId);
+
+        // THEN
+        assertThat(result).isNotEmpty();
+        assertThat(result.get()).hasNoNullFieldsOrProperties();
+    }
+
+    @Test
+    void getAllWarnsTest() {
+        // GIVEN
+        List<Warn> results;
+
+        // WHEN
+        results = repository.findAll();
+
+        // THEN
+        assertThat(results).isNotEmpty();
+    }
+
+    @Test
+    void getWarnsByMediaIdTest() {
+        // GIVEN
+        Long mediaId = 1L;
+        List<Warn> results;
+
+        // WHEN
+        results = repository.findAllByMediaId(mediaId);
+
+        // THEN
+        assertThat(results).isNotEmpty();
+    }
+
+    @Test
+    void deleteWarnByIdTest() {
+        // GIVEN
+        Long warnId = 1L;
+        Optional<Warn> resultBefore;
+        Optional<Warn> resultAfter;
+
+        // WHEN
+        resultBefore = repository.findById(warnId);
+        repository.deleteById(warnId);
+        resultAfter = repository.findById(warnId);
+
+        // THEN
+        assertThat(resultBefore).isNotEmpty();
+        assertThat(resultAfter).isEmpty();
+    }
+
 }
