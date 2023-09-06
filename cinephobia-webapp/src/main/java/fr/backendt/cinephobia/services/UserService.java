85057b1bb2e63e269fb039091036289954e5b7ea
Backendt <matisse.beckandt@gmail.com>
2023-09-06 19:24:52 +0200

    Added User Service and tests

---
+++ cinephobia-webapp/src/main/java/fr/backendt/cinephobia/services/UserService.java
@@ -0,0 +1,92 @@
+package fr.backendt.cinephobia.services;
+
+import fr.backendt.cinephobia.exceptions.ModelException;
+import fr.backendt.cinephobia.models.User;
+import fr.backendt.cinephobia.repositories.UserRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import static fr.backendt.cinephobia.exceptions.ModelException.ModelNotFoundException;
+import static java.util.concurrent.CompletableFuture.completedFuture;
+
+@Service
+public class UserService {
+
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Async
+    public CompletableFuture<User> createUser(User user) {
+        user.setId(null);
+        try {
+            User savedUser = repository.save(user);
+            return completedFuture(savedUser);
+        } catch(DataIntegrityViolationException exception) {
+            throw new ModelException("User with the same email already exists");
+        }
+    }
+
+    @Async
+    public CompletableFuture<List<User>> getUsers() {
+        List<User> users = repository.findAll();
+        return completedFuture(users);
+    }
+
+    @Async
+    public CompletableFuture<User> getUserById(Long id) {
+        User user = repository.findById(id)
+                .orElseThrow(() -> new ModelNotFoundException("User not found"));
+        return completedFuture(user);
+    }
+
+    @Async
+    public CompletableFuture<User> getUserByEmail(String email) {
+        User user = repository.findByEmail(email)
+                .orElseThrow(() -> new ModelNotFoundException("User not found"));
+        return completedFuture(user);
+    }
+
+    @Async
+    public CompletableFuture<User> replaceUserById(Long id, User user) {
+        boolean userExists = repository.existsById(id);
+        if(!userExists) {
+            throw new ModelNotFoundException("User not found");
+        }
+
+        user.setId(id);
+        User savedUser = repository.save(user);
+        return completedFuture(savedUser);
+    }
+
+    @Async
+    public CompletableFuture<User> updateUserById(Long id, User user) {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setSkipNullEnabled(true);
+
+        user.setId(null);
+        User savedUser = getUserById(id).join();
+        mapper.map(user, savedUser);
+
+        repository.save(savedUser);
+        return completedFuture(savedUser);
+    }
+
+    @Async
+    public void deleteUserById(Long id) {
+        boolean userExists = repository.existsById(id);
+        if(!userExists) {
+            throw new ModelNotFoundException("User not found");
+        }
+
+        repository.deleteById(id);
+    }
+
+}
---
+++ cinephobia-webapp/src/test/java/fr/backendt/cinephobia/services/UserServiceTests.java
@@ -0,0 +1,276 @@
+package fr.backendt.cinephobia.services;
+
+import fr.backendt.cinephobia.exceptions.ModelException;
+import fr.backendt.cinephobia.models.User;
+import fr.backendt.cinephobia.repositories.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.ArgumentMatchers.any;
+
+import static fr.backendt.cinephobia.exceptions.ModelException.ModelNotFoundException;
+import static org.mockito.Mockito.*;
+
+class UserServiceTests {
+
+    private UserRepository  repository;
+    private UserService service;
+
+    private User testUser;
+
+    @BeforeEach
+    void initTests() {
+        this.repository = Mockito.mock(UserRepository.class);
+        this.service = new UserService(repository);
+
+        testUser = new User("Jane Doe", "jane.doe@test.com", "myPassword1234", "USER");
+    }
+
+    @Test
+    void createUserTest() {
+        // GIVEN
+        User testUserWithId = new User(testUser);
+        testUserWithId.setId(1L);
+        User result;
+
+        when(repository.save(any())).thenReturn(testUserWithId);
+        // WHEN
+        result = service.createUser(testUserWithId).join();
+
+        // THEN
+        verify(repository).save(testUser);
+        assertThat(result).isNotNull();
+    }
+
+    @Test
+    void createDuplicatedUserTest() {
+        // GIVEN
+        when(repository.save(any()))
+                .thenThrow(DataIntegrityViolationException.class);
+        // WHEN
+        // THEN
+        assertThatExceptionOfType(ModelException.class)
+                .isThrownBy(() -> service.createUser(testUser).join());
+    }
+
+    @Test
+    void getUsersTest() {
+        // GIVEN
+        List<User> results;
+
+        when(repository.findAll()).thenReturn(List.of(testUser));
+        // WHEN
+        results = service.getUsers().join();
+
+        // THEN
+        verify(repository).findAll();
+        assertThat(results).containsExactly(testUser);
+    }
+
+    @Test
+    void getUserByIdTest() {
+        // GIVEN
+        Long userId = 1L;
+        User result;
+
+        when(repository.findById(any()))
+                .thenReturn(Optional.of(testUser));
+        // WHEN
+        result = service.getUserById(userId).join();
+
+        // THEN
+        verify(repository).findById(userId);
+        assertThat(result).isEqualTo(testUser);
+    }
+
+    @Test
+    void getUnknownUserByIdTest() {
+        // GIVEN
+        Long unknownUserId = 1L;
+
+        when(repository.findById(any()))
+                .thenReturn(Optional.empty());
+        // WHEN
+        // THEN
+        assertThatExceptionOfType(ModelException.ModelNotFoundException.class)
+                .isThrownBy(() -> service.getUserById(unknownUserId).join());
+    }
+
+    @Test
+    void getUserByEmailTest() {
+        // GIVEN
+        String userEmail = "user@test.com";
+        User result;
+
+        when(repository.findByEmail(any()))
+                .thenReturn(Optional.of(testUser));
+        // WHEN
+        result = service.getUserByEmail(userEmail).join();
+
+        // THEN
+        verify(repository).findByEmail(userEmail);
+        assertThat(result).isEqualTo(testUser);
+    }
+
+    @Test
+    void getUnknownUserByEmail() {
+        // GIVEN
+        String unknownEmail = "unknown@test.com";
+
+        when(repository.findByEmail(any()))
+                .thenReturn(Optional.empty());
+        // WHEN
+        // THEN
+        assertThatExceptionOfType(ModelException.ModelNotFoundException.class)
+                .isThrownBy(() -> service.getUserByEmail(unknownEmail).join());
+    }
+
+    @Test
+    void updateUserByIdTest() {
+        // GIVEN
+        Long userId = 1L;
+        User userUpdate = new User();
+
+        String newEmail = "new@test.com";
+        userUpdate.setEmail(newEmail);
+
+        User updatedUser = new User(testUser);
+        updatedUser.setEmail(newEmail);
+
+        User result;
+
+        when(repository.findById(any()))
+                .thenReturn(Optional.of(testUser));
+        when(repository.save(any()))
+                .thenReturn(updatedUser);
+        // WHEN
+        result = service.updateUserById(userId, userUpdate).join();
+
+        // THEN
+        verify(repository).findById(userId);
+        verify(repository).save(updatedUser);
+
+        assertThat(result).isEqualTo(updatedUser);
+    }
+
+    @Test
+    void updateUserByIdDoesntUpdateIdTest() {
+        // GIVEN
+        Long userId = 1L;
+
+        User userUpdate = new User();
+        Long newId = 2L;
+        userUpdate.setId(newId);
+        String newEmail = "new@test.com";
+        userUpdate.setEmail(newEmail);
+
+        User updatedUser = new User(testUser);
+        updatedUser.setEmail(newEmail);
+
+        User result;
+
+        when(repository.findById(any()))
+                .thenReturn(Optional.of(testUser));
+        when(repository.save(any()))
+                .thenReturn(updatedUser);
+        // WHEN
+        result = service.updateUserById(userId, userUpdate).join();
+
+        // THEN
+        verify(repository).findById(userId);
+        verify(repository).save(updatedUser);
+
+        assertThat(result).isEqualTo(updatedUser);
+    }
+
+    @Test
+    void updateUnknownUserByIdTest() {
+        // GIVEN
+        Long unknownUserId = 1L;
+
+        User userUpdate = new User();
+        userUpdate.setDisplayName("Not Found");
+
+        when(repository.findById(any()))
+                .thenReturn(Optional.empty());
+        // WHEN
+        // THEN
+        assertThatExceptionOfType(ModelNotFoundException.class)
+                .isThrownBy(() -> service.updateUserById(unknownUserId, userUpdate).join());
+
+        verify(repository).findById(unknownUserId);
+        verify(repository, never()).save(any());
+    }
+
+    @Test
+    void replaceUserByIdTest() {
+        // GIVEN
+        Long userId = 1L;
+        User newUser = new User("New User", "user@test.com", "newpassword", "USER");
+        User expectedUser = new User(newUser);
+        expectedUser.setId(userId);
+
+        User result;
+
+        when(repository.existsById(any())).thenReturn(true);
+        when(repository.save(any())).thenReturn(expectedUser);
+        // WHEN
+        result = service.replaceUserById(userId, newUser).join();
+
+        // THEN
+        verify(repository).existsById(userId);
+        verify(repository).save(expectedUser);
+        assertThat(result).isEqualTo(expectedUser);
+    }
+
+    @Test
+    void replaceUnknownUserByIdTest() {
+        // GIVEN
+        Long userId = 1L;
+
+        when(repository.existsById(any())).thenReturn(false);
+        // WHEN
+        // THEN
+        assertThatExceptionOfType(ModelNotFoundException.class)
+                .isThrownBy(() -> service.replaceUserById(userId, testUser).join());
+        verify(repository).existsById(userId);
+        verify(repository, never()).save(any());
+    }
+
+    @Test
+    void deleteUserByIdTest() {
+        // GIVEN
+        Long userId = 1L;
+
+        when(repository.existsById(any())).thenReturn(true);
+        // WHEN
+        service.deleteUserById(userId);
+
+        // THEN
+        verify(repository).existsById(userId);
+        verify(repository).deleteById(userId);
+    }
+
+    @Test
+    void deleteUnknownUserByIdTest() {
+        // GIVEN
+        Long userId = 1L;
+
+        when(repository.existsById(any())).thenReturn(false);
+        // WHEN
+        // THEN
+        assertThatExceptionOfType(ModelNotFoundException.class)
+                .isThrownBy(() -> service.deleteUserById(userId));
+
+        verify(repository).existsById(userId);
+        verify(repository, never()).deleteById(any());
+    }
+
+}
