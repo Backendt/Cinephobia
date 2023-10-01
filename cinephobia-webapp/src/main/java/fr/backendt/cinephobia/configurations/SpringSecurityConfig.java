b2767c6a1c2ccbb27f5899a896897e2517f5a838
Backendt <matisse.beckandt@proton.me>
2023-10-01 17:22:04 +0200

    Ditch REST API (i feel grief)

--- cinephobia-webapp/src/main/java/fr/backendt/cinephobia/configurations/SpringSecurityConfig.java
+++ cinephobia-webapp/src/main/java/fr/backendt/cinephobia/configurations/SpringSecurityConfig.java
@@ -3,8 +3,6 @@ package fr.backendt.cinephobia.configurations;
 import fr.backendt.cinephobia.services.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -32,11 +30,9 @@ public class SpringSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(requests -> requests
-                        .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
-                        .requestMatchers("/api/**").authenticated()
-                        .anyRequest().permitAll()
-                )
-                .formLogin(Customizer.withDefaults());
+                        .requestMatchers("/register", "/login", "/**.css", "/**.js").permitAll()
+                        .anyRequest().authenticated()
+                );
         return http.build();
     }
 
