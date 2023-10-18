1e01eeadae73d4feed6cbb609c5244973f4ee4bb
Backendt <matisse.beckandt@proton.me>
2023-10-19 01:01:25 +0200

    Renamed HomeController to StaticController, added html home page

---
+++ cinephobia-webapp/src/main/java/fr/backendt/cinephobia/controllers/StaticController.java
@@ -0,0 +1,14 @@
+package fr.backendt.cinephobia.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StaticController {
+
+    @GetMapping("/")
+    public String getHome() {
+        return "home";
+    }
+
+}
