deb5175361d06da7ad087440a61da0fd1cb2bcb1
Backendt <matisse.beckandt@proton.me>
2023-10-19 20:01:37 +0200

    Added more media test values

--- cinephobia-webapp/src/test/resources/database.sql
+++ cinephobia-webapp/src/test/resources/database.sql
@@ -52,7 +52,7 @@ CREATE TABLE IF NOT EXISTS users_warns(
 );
 
 INSERT INTO platform(name) VALUES ('Netflix'), ('Prime Video'), ('Disney+'), ('Cinema'), ('Other');
-INSERT INTO media(title, image_url) VALUES ('Cinephobia: The Revenge', 'https://example.com/cinephobia.png');
+INSERT INTO media(title, image_url) VALUES ('Cinephobia: The Revenge', 'https://example.com/cinephobia.png'), ('Lorem ipsum', 'https://example.com/'), ('More lorem, less ipsum', 'https://example.com');
 INSERT INTO media_platforms(platform_id, media_id) VALUES (1, 1);
 INSERT INTO triggr(name, description) VALUES ('Testphobia', 'Fear of unit tests failing'), ('Bugphobia', 'Fear of software bugs');
 INSERT INTO warn(triggr_id, media_id, exposition_level) VALUES (2, 1, 9);
