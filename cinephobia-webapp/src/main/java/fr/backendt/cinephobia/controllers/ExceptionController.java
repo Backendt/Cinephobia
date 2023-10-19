b97e894bf1d495f759174a4326806b4595e9ae8c
Backendt <matisse.beckandt@proton.me>
2023-10-19 03:16:45 +0200

    Added error message in logs

--- cinephobia-webapp/src/main/java/fr/backendt/cinephobia/controllers/ExceptionController.java
+++ cinephobia-webapp/src/main/java/fr/backendt/cinephobia/controllers/ExceptionController.java
@@ -19,7 +19,7 @@ public class ExceptionController {
 
     @ExceptionHandler(Exception.class)
     public String catchExceptions(Model model, Exception exception) {
-        LOGGER.error(exception);
+        LOGGER.error("An error occurred", exception);
         String errorMessage = "Sorry, an unexpected error occurred! Please try again later";
         model.addAttribute("err", errorMessage);
         return "error";
