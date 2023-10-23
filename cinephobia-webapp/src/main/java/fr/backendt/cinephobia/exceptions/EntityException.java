8a89f9a5f58104a5f8da7cce68985023bec19d8a
Backendt <matisse.beckandt@proton.me>
2023-10-23 18:03:53 +0200

    Removed exception controller

--- cinephobia-webapp/src/main/java/fr/backendt/cinephobia/exceptions/EntityException.java
+++ cinephobia-webapp/src/main/java/fr/backendt/cinephobia/exceptions/EntityException.java
@@ -1,11 +1,16 @@
 package fr.backendt.cinephobia.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class EntityException extends RuntimeException {
 
     public EntityException(String message) {
         super(message);
     }
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public static class EntityNotFoundException extends EntityException {
 
         public EntityNotFoundException(String message) {
