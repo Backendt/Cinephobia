5354890e0730f3f548f0d3df3c7103dd7e32aef3
Backendt <matisse.beckandt@proton.me>
2024-07-13 21:28:36 +0200

    Added a simplified warn DTO

---
+++ cinephobia-webapp/src/main/java/fr/backendt/cinephobia/models/dto/WarnResponseDTO.java
@@ -0,0 +1,28 @@
+package fr.backendt.cinephobia.models.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
+
+@Data
+@Generated
+@AllArgsConstructor
+@NoArgsConstructor
+public class WarnDTO {
+
+    private Long id;
+
+    @NotNull(message = "The media is required")
+    private Long mediaId;
+
+    @NotNull(message = "The trigger is required")
+    private TriggerDTO trigger;
+
+    @NotNull(message = "The exposition level is required")
+    @Range(min = 1, max = 10, message = "The exposition level must be between 0 and 10")
+    private int expositionLevel;
+
+}
